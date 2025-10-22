461970bd79b05fd6c1ac4167eb75f0d75fa01696
jlgsjlgs <jonathanlimgs@hotmail.com>
2025-10-22 21:49:24 +0800

    initial commit

---
+++ src/main/java/com/jlgs/howmuchah/HowmuchahApplication.java
@@ -0,0 +1,13 @@
+package com.jlgs.howmuchah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowmuchahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowmuchahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlgs/howmuchah/HowmuchahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlgs.howmuchah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowmuchahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
